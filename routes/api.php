c7238073bda2746533e4ab296cefdcf822e571f0
Abdugafur111 <64993553+Abdugafur111@users.noreply.github.com>
2023-03-14 05:46:43 -0700

    ai

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::get('/products',[ProductController::class,'index']);
 Route::get('/products/{id}',[ProductController::class,'show']);
 Route::get('/products/search/{name}',[ProductController::class,'search']);
 Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
 
 
  
